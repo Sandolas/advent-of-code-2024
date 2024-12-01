c649b76bc373bebfce13427062a29e82c704802e
Sandolas <aaron-s@gmx.net>
2024-12-01 10:30:11 +0100

    Add solution for the second part of day 1

--- day1.py
+++ day1.py
@@ -1,18 +1,30 @@
-import csv
+def main():
+    leftList = []
+    rightList = []
+    with open("input.tmp") as inputfile:
+        for line in inputfile:
+           leftNumberStr, rightNumberStr = line.strip().split("   ")
+           leftList.append(int(leftNumberStr))
+           rightList.append(int(rightNumberStr))
 
-leftList = []
-rightList = []
-with open("input.tmp") as inputfile:
-    for line in inputfile:
-       leftNumberStr, rightNumberStr = line.strip().split("   ")
-       leftList.append(int(leftNumberStr))
-       rightList.append(int(rightNumberStr))
+    leftList.sort()
+    rightList.sort()
 
-leftList.sort()
-rightList.sort()
+    differenceSum = 0
+    for (leftNumber, rightNumber) in zip(leftList, rightList):
+        differenceSum += abs(leftNumber - rightNumber)
 
-differenceSum = 0
-for (leftNumber, rightNumber) in zip(leftList, rightList):
-    differenceSum += abs(leftNumber - rightNumber)
+    similarityScore = getSimilarityScore(leftList, rightList)
 
-print(differenceSum)
+    print(f"Sum of differences: {differenceSum}")
+    print(f"Similarity score:   {similarityScore}")
+
+def getSimilarityScore(a, b):
+    score = 0
+    for element in a:
+        occurrencesInB = b.count(element)
+        score += element * occurrencesInB
+
+    return score
+
+main()
