87f1dfdec8f00140b4b7d2a11e1913666009e920
Sandolas <56173519+Sandolas@users.noreply.github.com>
2024-12-29 19:49:02 +0100

    Refactor day 3 solution

--- day3.py
+++ day3.py
@@ -2,22 +2,23 @@ import re
 import sys
 
 def main():
-    mulExpressionList = []
-    with open(sys.argv[1]) as inputfile:
-        inputString = inputfile.read()
-
-    mulPattern = r"mul\([0-9]{1,3},[0-9]{1,3}\)"
     enableString  = "do()"
     disableString = "don't()"
+    mulPattern = r"mul\([0-9]{1,3},[0-9]{1,3}\)"
 
+    expressionList = []
     enablePattern  = re.escape(enableString)
     disablePattern = re.escape(disableString)
     validExpressionPattern = f"{mulPattern}|{enablePattern}|{disablePattern}"
-    mulExpressionList += re.findall(validExpressionPattern, inputString)
+
+    with open(sys.argv[1]) as inputfile:
+        inputString = inputfile.read()
+
+    expressionList += re.findall(validExpressionPattern, inputString)
 
     multiplicationSum = 0
     currentlyEnabled = True
-    for expression in mulExpressionList:
+    for expression in expressionList:
         if expression == enableString:
             currentlyEnabled = True
         elif expression == disableString:
